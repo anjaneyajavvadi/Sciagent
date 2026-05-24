b039e51dbcc1dd697c88c77667733bbdc9dbfd94
anjaneyajavvadi <anjaneyajavvadi@gmail.com>
2026-05-24 21:30:57 +0530

    updates in agent state completed

--- app/graph/state.py
+++ app/graph/state.py
@@ -1,13 +1,15 @@
-from langgraph.graph import MessagesState
-from typing import List,Dict,Any
+from typing import List, Dict, Any
 from typing_extensions import TypedDict
 
 
-class AgentState:
-    query:str
-    retrieved_chunks:List[Dict[str,Any]]
-    reranked_chunks:List[Dict[str,Any]]
-    compresses_context:str
-    answer:str
-    sources:List[str]
-    web_search_used:bool
+class AgentState(TypedDict):
+    query:              str
+    sub_questions:      List[str]        
+    retrieved_chunks:   List[Dict[str, Any]]
+    reranked_chunks:    List[Dict[str, Any]]
+    compressed_context: str
+    reflection:         str              
+    answer:             str
+    sources:            List[str]
+    web_search_used:    bool
+    iteration_count:    int              
